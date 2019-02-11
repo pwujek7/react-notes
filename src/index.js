36d1d89f26da2db222d384f962b4c060d4f81f7a
PW <pwujek7@gmail.com>
2019-02-11 19:42:24 +0100

    initial project structure configuration

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const ROOT = document.getElementById('root');
+
+const App = () => (
+  <div>
+    <h1>React notes project</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, ROOT);
