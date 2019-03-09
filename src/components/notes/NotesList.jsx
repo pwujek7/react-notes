29d255577d731fee98bccec623a577f7561f2ac8
PW <pwujek7@gmail.com>
2019-03-09 12:14:09 +0100

    added styled-components dependency & basic styles

--- src/components/notes/NotesList.jsx
+++ src/components/notes/NotesList.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import NoteItem from './NoteItem';
 
 import { getFilteredNotes } from '../../utilities/notes';
 
+const NotesGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+`;
+
 const NotesList = ({ notes }) => (
-  <div>
+  <NotesGrid>
     {
       notes && notes.map(note => (
         <NoteItem key={note.id} note={note} />
       ))
     }
-  </div>
+  </NotesGrid>
 );
 
 NotesList.propTypes = {
--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import {
 } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import styled, { createGlobalStyle } from 'styled-components';
+
 import store from './store/store';
 
 import Navigation from './components/navigation/Navigation';
@@ -14,21 +16,38 @@ import Home from './components/home/Home';
 import NotesList from './components/notes/NotesList';
 import AddNote from './components/addNote/AddNote';
 
-const ROOT = document.getElementById('root');
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: 25% 75%;
+  height: 100vh;
+`;
 
 const App = () => (
   <HashRouter>
     <Fragment>
-      <Navigation />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/add-note/:noteId" component={AddNote} />
-        <Route path="/add-note" component={AddNote} />
-        <Route path="/:filter" component={NotesList} />
-      </Switch>
+      <GlobalStyle />
+      <GridContainer>
+        <Navigation />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/add-note/:noteId" component={AddNote} />
+          <Route path="/add-note" component={AddNote} />
+          <Route path="/:filter" component={NotesList} />
+        </Switch>
+      </GridContainer>
     </Fragment>
   </HashRouter>
 );
+
+const ROOT = document.getElementById('root');
 
 ReactDOM.render(
   <Provider store={store}>
